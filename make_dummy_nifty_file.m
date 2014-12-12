9e0e708ef15a80ebcb98bfda8fbaf9c2823d17d1
mosszhaodphil <mosszhao@Mosss-MacBook-Pro-2.local>
2014-12-12 22:16:31 +0000

    comment header modification
    only matrix value changed, header information remains unchanged

--- make_dummy_nifty_file.m
+++ make_dummy_nifty_file.m
@@ -7,8 +7,8 @@ function dummy_file_handle = make_dummy_nifty_file(sample_file_handle)
 
 	dummy_file_handle = sample_file_handle;
 
-	dummy_file_handle.hdr.dime.dim(2 : 5) = 0; % set dimension of x, y, z, t to zero
-	dummy_file_handle.hdr.dime.pixdim(2 : 5) = 0; % set pixel dimension of x, y, z, t to zero
+	%dummy_file_handle.hdr.dime.dim(2 : 5) = [64, 64, 4, 20]; % set dimension of x, y, z, t to zero
+	%dummy_file_handle.hdr.dime.pixdim(2 : 5) = 0; % set pixel dimension of x, y, z, t to zero
 	dummy_file_handle.img(:) = 0; % erase all the values in signal matrix
 
 end
